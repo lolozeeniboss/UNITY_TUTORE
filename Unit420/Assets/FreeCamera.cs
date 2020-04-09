2ccc240785cdb3fba7d18a620d3fa9c312693fe7
Thomas <lowlifeagain123@gmail.com>
2020-04-09 19:19:58 +0200

    Caméra libre fonctionnel
    2 Caméras tournent simultanément
    Button -> pas fonctionnel doit permettre de rendre actif 1 caméra

---
+++ Unit420/Assets/FreeCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeCamera : MonoBehaviour
+{
+   
+    public float panSpeed = 20f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        float moveAmount = 100f;
+        float edgeSize = 30f;
+        if (Input.mousePosition.x > Screen.width - edgeSize)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x < edgeSize)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.y > Screen.height - edgeSize)
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.y < edgeSize)
+        {
+            pos.y -= panSpeed * Time.deltaTime;
+        }
+        transform.position = pos;
+    }
+}
