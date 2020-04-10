8101ffee5793853949ce4d5d6285c8c21626767e
zen-PC_FIXE <loris.pistilli.iut@gmail.com>
2020-04-11 01:18:58 +0200

    debut de menu
    menu principale qui peut lancer la demo et quitter le jeu (scene: Menu) menu de pause sur la V2 pour l'instant uniquement activable avec la touche echap sur pc

---
+++ Unit420/Assets/PauseMenu.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }    
+    }
+
+    void Pause()
+    {
+        Debug.Log("pause");
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        Debug.Log("reprendre");
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Menu()
+    {
+        Debug.Log("retour au menu");
+        Time.timeScale = 1f; //enleve la pause du jeu si on reviens au menu pour ne pas avoir un menu freeser
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quitter");
+        Application.Quit();
+    }
+
+}
